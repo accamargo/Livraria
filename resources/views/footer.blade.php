774ae82be6eaf0d5ce40b2480a6894b5c5afbb84
Andressa Camargo <dessaccamargo@gmail.com>
2018-11-25 03:38:43 -0200

    Add footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<div class="fixed-bottom">
+    <div class="jumbotron">
+        <h3>OrbisBooks</h3>
+        <p>"Há livros escritos para evitar espaços vazios na estante" - Carlos Dummond </p>
+        <hr class="my-4">
+        <h5>Informações:</h5>
+        <ul class="list-inline">
+            <li  class="list-inline-item"><a href="https://www.google.com/">Google</a></li>
+        </ul>
+    </div>
+</div>
+</html>
