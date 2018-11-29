84e760aada4ee8097949ed9f55c25424527f3ba3
Andressa Camargo <dessaccamargo@gmail.com>
2018-11-29 01:11:23 -0200

    migrate customer

--- database/migrations/2018_11_29_025950_create_bookcustomers_table.php
+++ database/migrations/2018_11_29_025950_create_bookcustomers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateBookcustomersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
+        Schema::create('bookcustomers', function (Blueprint $table) {
+            $table->increments('custID');
+            $table->string('fname');
+            $table->string('lname');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('street');
+            $table->string('city');
+            $table->string('state');
+            $table->string('zip');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -31,6 +35,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('bookcustomers');
     }
 }
